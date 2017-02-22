b7c2290750c0fbd72c9a33cf2718aa92bbaede33
laaaders <larsmade@stud.ntnu.no>
2017-02-22 15:09:31 +0100

    Added a query example in the files "Run" and "Login"

--- src/layout/LoginController.java
+++ src/layout/LoginController.java
@@ -7,7 +7,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
