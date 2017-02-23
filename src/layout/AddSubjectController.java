90b7ba945e9c28db8c50167342b24c8a0bb0bcc2
laaaders <larsmade@stud.ntnu.no>
2017-02-23 10:46:11 +0100

    La til et addSubjectcontroller

---
+++ src/layout/AddSubjectController.java
@@ -0,0 +1,38 @@
+package layout;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by larsmade on 23.02.2017.
+ */
+public class AddSubjectController {
+
+    @FXML
+    TextField subject;
+
+    @FXML
+    JFXButton sendInn;
+
+
+
+
+
+
+    public void addSubjectToCalendar(){
+
+        String subjectName = subject.getText();
+
+        //check up against database..
+
+        //Then add it to calendar if it exist.
+
+        System.out.println("yolo");
+
+    }
+
+
+
+
+}
