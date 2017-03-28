409bc000b5e85901993776b14d16753c34e34bd5
torresrl <torres.lande@gmail.com>
2017-03-28 13:37:59 +0200

    lagt til farger på uke eventer/aktivitys

--- src/layout/eventButton.java
+++ src/layout/eventButton.java
@@ -5,11 +5,18 @@ import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static java.awt.Color.red;
 
 /**
  * Created by torresrl on 22/03/2017.
@@ -29,9 +36,11 @@ public class eventButton {
         event = new Button();
         event.setId("eventButton");
         event.wrapTextProperty().setValue(true);
-        event.setText("Activity\n" +name+ "\n\nDeskription\n" + description);
+        event.setText(name+ "\n\n" + description);
         event.setMaxHeight(Double.MAX_VALUE);
         event.setMaxWidth(Double.MAX_VALUE);
+        event.setStyle("-fx-background-color: #" + ranColor());
+
 
         //vis du tryker på et event kommer mer info opp:
         event.setOnAction( e -> {
@@ -69,9 +78,10 @@ public class eventButton {
         event = new Button();
         event.setId("eventButton");
         event.wrapTextProperty().setValue(true);
-        event.setText("Activity\n" +name+ "\n\nDeskription\n" + description);
+        event.setText(name+ "\n\n" + description);
         event.setMaxHeight(Double.MAX_VALUE);
         event.setMaxWidth(Double.MAX_VALUE);
+        event.setStyle("-fx-background-color: #" +ranColor());
 
         //vis du tryker på et event kommer mer info opp:
         event.setOnAction( e -> {
@@ -115,6 +125,12 @@ public class eventButton {
     public String getName() {return name;}
 
     public String getDescription() {return description;}
+
+    private String ranColor(){
+        String [] colors = {"00F000","FF0000","00FF00","0000FF","FFFF00","00FFFF","FF00FF","C0C0C0"};
+        int randomNum = ThreadLocalRandom.current().nextInt(0,  7);
+        return colors[randomNum];
+    }
 
 
 
