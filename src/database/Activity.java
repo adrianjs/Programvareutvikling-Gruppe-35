8f195e88a3655feb465705559fc3d07d41b583ae
torresrl <torres.lande@gmail.com>
2017-04-17 12:34:47 +0200

    har nå testet add og remvove activity

--- src/database/Activity.java
+++ src/database/Activity.java
@@ -1,6 +1,9 @@
 package database;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * Created by torresrl on 30/03/2017.
@@ -19,4 +22,29 @@ public class Activity extends Connect {
             se.printStackTrace();
         }
     }
+
+    public ArrayList getLastActivity(){
+        ArrayList outData = new ArrayList();
+        try{
+            ResultSet data = stmt.executeQuery("SELECT * FROM ACTIVITY ORDER BY activityID DESC LIMIT 1");
+            while(data.next()){
+                outData.add(data.getInt(1));
+                outData.add(data.getString(2));
+                outData.add(data.getDate(3));
+                outData.add(data.getInt(4));
+                outData.add(data.getDouble(5));
+                outData.add(data.getDouble(6));
+                outData.add(data.getInt(7));
+                outData.add(data.getString(8));
+                outData.add(data.getString(9));
+                outData.add(data.getString(10));
+            }
+        } catch (SQLException se){
+            se.printStackTrace();
+        }
+        return outData;
+    }
+
 }
+
+
---
+++ tests/database/activityTest.java
@@ -0,0 +1,52 @@
+package database;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ * Created by torresrl on 17/04/2017.
+ */
+public class activityTest {
+    //TODO hvorfor virker kun add med larsmade@stud.ntnu.no?
+
+    @Test
+    public void testAddAndRemove(){
+
+        ArrayList lastAddedActivity;
+        int IdTestActivity;
+        Date date = new Date(2017-04-17);
+        Connect add = new Connect();
+        add.addActivity("Activity test", date, false, 1, 13.0, 14.0, "larsmade@stud.ntnu.no",
+                "denne activiteten er kun for testting hvis den ligger i databasen har noe gale skjedd og den skal slettes");
+        add.close();
+
+        Activity getTest = new Activity();
+        lastAddedActivity = getTest.getLastActivity();
+        assertEquals("Activity test", lastAddedActivity.get(1));
+        assertEquals(0, lastAddedActivity.get(3));
+        assertEquals(13.0, lastAddedActivity.get(4));
+        assertEquals(14.0, lastAddedActivity.get(5));
+        assertEquals(1, lastAddedActivity.get(6));
+        assertEquals("larsmade@stud.ntnu.no", lastAddedActivity.get(7));
+        assertEquals("denne activiteten er kun for testting hvis den ligger i databasen har noe gale skjedd " +
+                "og den skal slettes", lastAddedActivity.get(8));
+        IdTestActivity = (int) lastAddedActivity.get(0);
+
+        getTest.delteActivity(IdTestActivity);
+        lastAddedActivity = getTest.getLastActivity();
+        assertNotEquals(IdTestActivity, lastAddedActivity.get(0));
+        getTest.close();
+
+
+
+
+    }
+
+}
